db8cb7845e226ecce06c373f688cd38e6abe353f
AbdimalikSec <saacidyuusuf871@gmail.com>
2026-01-24 18:30:42 +0300

    changes

--- api/index.py
+++ api/index.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import joblib
+import os
 
 app = FastAPI()
+
+# Load model ONCE when server starts
+MODEL_PATH = os.path.join(
+    os.path.dirname(__file__),
+    "..",
+    "model",
+    "student_model.joblib"
+)
+
+model = joblib.load(MODEL_PATH)
 
 class Input(BaseModel):
     weekly_self_study_hours: float
@@ -14,9 +26,10 @@ def health():
 
 @app.post("/predict")
 def predict(data: Input):
-    predicted_score = (
-        data.weekly_self_study_hours * 2
-        + data.attendance_percentage * 0.5
-        + data.class_participation * 5
-    )
-    return {"predicted_total_score": predicted_score}
+    X = [[
+        data.weekly_self_study_hours,
+        data.attendance_percentage,
+        data.class_participation
+    ]]
+    prediction = model.predict(X)[0]
+    return {"predicted_total_score": float(prediction)}
